35cf658d67fee76e24259ed012b7b69286049a00
Alex Strappazzon <strappazzonalex@gmail.com>
2025-09-15 21:19:29 +0200

    update who we are page

--- pages/whoWeAre.js
+++ pages/whoWeAre.js
@@ -17,9 +17,7 @@ import Parallax from "/components/Parallax/Parallax.js";
 import styles from "/styles/jss/nextjs-material-kit/pages/landingPage.js";
 
 // Sections for this page
-import ProductSection from "/pages-sections/LandingPage-Sections/ProductSection.js";
-import TeamSection from "/pages-sections/LandingPage-Sections/TeamSection.js";
-import WorkSection from "/pages-sections/LandingPage-Sections/WorkSection.js";
+import DescriptionSection from "/pages-sections/WhoWeAre-Sections/DescriptionSection.js";
 
 const dashboardRoutes = [];
 
@@ -41,7 +39,7 @@ export default function LandingPage(props) {
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
-          height: 320,
+          height: 50,
           color: "white",
         }}
         {...rest}
@@ -60,11 +58,12 @@ export default function LandingPage(props) {
           </GridContainer>
         </div>
       </Parallax>
-      <div className={classNames(classes.main, classes.mainRaised)}>
+      <div
+        className={classNames(classes.main, classes.mainRaised)}
+        style={{ margin: "-680px 30px 0px" }}
+      >
         <div className={classes.container}>
-          <ProductSection />
-          <TeamSection />
-          <WorkSection />
+          <DescriptionSection/>
         </div>
       </div>
       <Footer />
